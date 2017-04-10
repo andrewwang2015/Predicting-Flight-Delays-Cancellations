c7cbf15775e9fce4a9a22c0234290db5547d656a
David Kawashima <dkawashi@caltech.edu>
2017-04-09 23:58:33 -0700

    Added weather data pulling file

---
+++ scrape_weather_data.py
@@ -0,0 +1,45 @@
+"""
+Example script that scrapes data from the IEM ASOS download service
+"""
+import json
+import datetime
+from urllib.request import urlopen
+
+# timestamps in UTC to request data for
+startts = datetime.datetime(2015, 1, 1)
+endts = datetime.datetime(2015, 12, 31)
+
+SERVICE = "https://mesonet.agron.iastate.edu/cgi-bin/request/asos.py?"
+SERVICE += "data=all&tz=Etc/UTC&format=comma&latlon=yes&"
+
+SERVICE += startts.strftime('year1=%Y&month1=%m&day1=%d&')
+SERVICE += endts.strftime('year2=%Y&month2=%m&day2=%d&')
+
+states = """AK AL AR AZ CA CO CT DE FL GA HI IA ID IL IN KS KY LA MA MD ME
+ MI MN MO MS MT NC ND NE NH NJ NM NV NY OH OK OR PA RI SC SD TN TX UT VA VT
+ WA WI WV WY"""
+# IEM quirk to have Iowa AWOS sites in its own labeled network
+networks = ['AWOS']
+for state in states.split():
+    networks.append("%s_ASOS" % (state,))
+
+for network in networks:
+    # Get metadata
+    uri = ("https://mesonet.agron.iastate.edu/"
+           "geojson/network/%s.geojson") % (network,)
+    init_data = urlopen(uri).read()
+    data = init_data.decode('utf-8')
+    print (data[0])
+    jdict = json.loads(data)
+    for site in jdict['features']:
+        faaid = site['properties']['sid']
+        sitename = site['properties']['sname']
+        uri = '%s&station=%s' % (SERVICE, faaid)
+        print ('Network: %s Downloading: %s [%s]' % (network, sitename, faaid))
+        init_data = urlopen(uri).read()
+        data = init_data.decode('utf-8')
+        outfn = '%s_%s_%s.txt' % (faaid, startts.strftime("%Y%m%d%H%M"),
+                                  endts.strftime("%Y%m%d%H%M"))
+        out = open(outfn, 'w')
+        out.write(data)
+        out.close()
